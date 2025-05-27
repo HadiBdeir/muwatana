74ea574e89f850f00d20a1ab3be71074430d5880
Fatima Carolina Bdeir <fcb@Fatimas-MacBook-Air.local>
2025-05-27 00:20:35 -0400

    Switch to HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./HomeScreen";
 import ResourcesCyberSecurityAndPrivacy from "./Resources_CybersecurityAndPrivacy";
 import AccountProtectionScreen from "./Account_Protection_Screen";
